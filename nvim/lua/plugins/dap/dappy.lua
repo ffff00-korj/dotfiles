9b9c908843c6e64a17383886757733397e57c3b4
Ilya <borovkovkin@gmail.com>
2025-06-18 12:35:28 +0300

    add nvim dap py attach config

--- nvim/lua/plugins/dap/dappy.lua
+++ nvim/lua/plugins/dap/dappy.lua
@@ -3,6 +3,21 @@ local opts = {
   ft = "python",
   config = function()
     require("dap-python").setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
+    table.insert(require("dap").configurations.python, {
+      type = "python",
+      name = "attach to debugpy",
+      request = "attach",
+      connect = {
+        host = "127.0.0.1",
+        port = 5678,
+      },
+      pathMappings = {
+        {
+          localRoot = vim.fn.getcwd(),
+          remoteRoot = "/app",
+        },
+      },
+    })
   end,
 }
 
