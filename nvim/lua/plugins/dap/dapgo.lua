5916f13802f5f37208488da2d4802709323fb443
Ilya Borovkov <ilborovkov@ozon.ru>
2026-02-01 17:16:29 +0300

    ref

--- nvim/lua/plugins/dap/dapgo.lua
+++ nvim/lua/plugins/dap/dapgo.lua
@@ -8,16 +8,6 @@ local opts = {
       },
     })
 
-    local get_arguments = function()
-      local args_str = vim.fn.input("Arguments: ")
-      return vim.split(args_str, " ")
-    end
-
-    local get_build_flags = function()
-      local flags_str = vim.fn.input("Build flags: ")
-      return vim.split(flags_str, " ")
-    end
-
     local filtered_pick_process = function()
       return require("dap.utils").pick_process({
         filter = function(proc)
@@ -35,17 +25,17 @@ local opts = {
       },
       {
         type = "go",
+        name = "Test",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
+      },
+      {
+        type = "go",
         name = "Attach",
         mode = "local",
         request = "attach",
         processId = filtered_pick_process,
-      },
-      {
-        type = "go",
-        name = "Test (go.mod)",
-        request = "launch",
-        mode = "test",
-        program = "./${relativeFileDirname}",
       },
     }
   end,
