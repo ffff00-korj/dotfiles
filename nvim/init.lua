8580f043c417f8e54cd52ca72e3ccaf7cde1a9c1
Ilya <borovkovkin@gmail.com>
2026-03-07 22:03:45 +0300

    add some helpers for neovim and zshrc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
 -- config
 require("config.lazy")
+require("config.helpers")
 require("config.keymaps")
 require("config.options")
 require("config.autocmds")
---
+++ nvim/lua/config/helpers.lua
@@ -0,0 +1,9 @@
+function LoadSecretFromPass(secret_name)
+  local handle = io.popen("pass " .. secret_name)
+  if not handle then
+    error("Failed to load secret '" .. secret_name .. "'. Error: handle cannot be created")
+  end
+  local result = handle:read("*a")
+  handle:close()
+  return result:gsub("\n", "")
+end
