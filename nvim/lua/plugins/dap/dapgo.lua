e2aa2c60e97ec955ba8eeef8c2418b8f3e9be875
Ilya Borovkov <ilborovkov@ozon.ru>
2026-08-29 03:38:50 +0300

    add debug nearest test

--- nvim/lua/plugins/dap/dapgo.lua
+++ nvim/lua/plugins/dap/dapgo.lua
@@ -15,6 +15,24 @@ local opts = {
         end,
       })
     end
+    local function get_nearest_test_name()
+      local node = vim.treesitter.get_node()
+      while node do
+        local type = node:type()
+        if type == "function_declaration" or type == "method_declaration" then
+          local name_nodes = node:field("name")
+          if name_nodes and #name_nodes > 0 then
+            local name_node = name_nodes[1]
+            local name = vim.treesitter.get_node_text(name_node, 0)
+            if name:match("^Test") then
+              return name
+            end
+          end
+        end
+        node = node:parent()
+      end
+      return nil
+    end
 
     require("dap").configurations.go = {
       {
@@ -29,6 +47,21 @@ local opts = {
         request = "launch",
         mode = "test",
         program = "./${relativeFileDirname}",
+      },
+      {
+        type = "go",
+        name = "Nearest Test",
+        request = "launch",
+        mode = "test",
+        program = "./${relativeFileDirname}",
+        args = function()
+          local test_name = get_nearest_test_name()
+          if test_name == "" then
+            vim.notify("No test found under cursor!", vim.log.levels.ERROR)
+            return nil
+          end
+          return { "-test.run", "^" .. test_name .. "$" }
+        end,
       },
       {
         type = "go",
