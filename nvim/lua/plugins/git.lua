45214c742a0099fb134fa78f0961afcd9fdccecc
Ilya Borovkov <ilborovkov@ozon.ru>
2026-09-02 12:41:36 +0300

    add buffer reset command

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -22,6 +22,7 @@ local opts = {
             { buffer = bufnr, desc = "Gitsigns: [h]unk [p]review inline" }
           )
           vim.keymap.set("n", "<leader>hr", gs.reset_hunk, { buffer = bufnr, desc = "Gitsigns: [h]unk [r]eset" })
+          vim.keymap.set("n", "<leader>br", gs.reset_buffer, { buffer = bufnr, desc = "Gitsigns: [b]uffer[r]eset" })
         end,
       })
     end,
