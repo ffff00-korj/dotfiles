7de745bda630e9575711e261f8a6ecdb4760e66f
Ilya <borovkovkin@gmail.com>
2026-09-02 23:44:05 +0300

    change to mercury api

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -18,9 +18,9 @@ return {
           },
         },
         provider = {
-          -- cloud AI model https://platform.inceptionlabs.ai/
-          -- type = "mercuryapi",
-          -- api_key_env = "AUTOCOMPLETE_AI_TOKEN",
+          -- (BEST) cloud AI model https://platform.inceptionlabs.ai/
+          type = "mercuryapi",
+          api_key_env = "AUTOCOMPLETE_AI_TOKEN",
 
           -- install llama-server
           -- brew install llama.cpp
@@ -28,8 +28,8 @@ return {
           -- llama-server -hf sweepai/sweep-next-edit-0.5B --port 8000
           -- llama-server -hf sweepai/sweep-next-edit-1.5B --port 8000
           -- llama-server -hf sweepai/sweep-next-edit-v2-7B --port 8000
-          type = "sweep",
-          url = "http://localhost:8000",
+          -- type = "sweep",
+          -- url = "http://localhost:8000",
         },
       })
     end,
